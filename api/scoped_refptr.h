2777b785d3ddc6c9a4030bd4d6913dad959a39ad
leoagle <leoagle@users.noreply.github.com>
2020-08-19 17:01:35 +0800

    Update scoped_refptr.h
    1

--- api/scoped_refptr.h
+++ api/scoped_refptr.h
@@ -72,31 +72,33 @@ template <class T>
 class scoped_refptr {
  public:
   typedef T element_type;
-
+  //默认构造 传入空的引用计数对象
   scoped_refptr() : ptr_(nullptr) {}
-
+  //构造 传入引用计数对象p的指针，调用其AddRef()方法 引用计数+1
   scoped_refptr(T* p) : ptr_(p) {  // NOLINT(runtime/explicit)
     if (ptr_)
       ptr_->AddRef();
   }
-
+  //拷贝构造，对应的引用计数+1，调用引用计数对象的AddRef()方法
   scoped_refptr(const scoped_refptr<T>& r) : ptr_(r.ptr_) {
     if (ptr_)
       ptr_->AddRef();
   }
-
+  //拷贝构造，U必须是T的子类
   template <typename U>
   scoped_refptr(const scoped_refptr<U>& r) : ptr_(r.get()) {
     if (ptr_)
       ptr_->AddRef();
   }
-
+  //移动构造(右值引用)，表示对象的转移，亦即使用同一个对象.因此，需要保持对引用计数对象的引用次数不变
+  //所以此处调用scoped_refptr<T>的release()方法，将原来的scoped_refptr<T>对象内部引用计数指针置空
+  //新的scoped_refptr<T>对象来保存引用计数对象，以达到转移的目的
   // Move constructors.
   scoped_refptr(scoped_refptr<T>&& r) noexcept : ptr_(r.release()) {}
-
+  //移动构造，U必须是T的子类
   template <typename U>
   scoped_refptr(scoped_refptr<U>&& r) noexcept : ptr_(r.release()) {}
-
+  //析构函数，调用引用计数对象Release()，引用计数-1
   ~scoped_refptr() {
     if (ptr_)
       ptr_->Release();
@@ -105,7 +107,7 @@ class scoped_refptr {
   T* get() const { return ptr_; }
   operator T*() const { return ptr_; }
   T* operator->() const { return ptr_; }
-
+  //不可轻易调用.会使智能指针内部存储的ptr_为空,失去对引用计数对象的引用,而引用计数没有-1
   // Returns the (possibly null) raw pointer, and makes the scoped_refptr hold a
   // null pointer, all without touching the reference count of the underlying
   // pointed-to object. The object is still reference counted, and the caller of
@@ -116,7 +118,7 @@ class scoped_refptr {
     ptr_ = nullptr;
     return retVal;
   }
-
+  //重载赋值运算符.赋值新的引用计数对象的指针，新引用计数对象的引用计数+1，原来的-1
   scoped_refptr<T>& operator=(T* p) {
     // AddRef first so that self assignment should work
     if (p)
@@ -126,33 +128,35 @@ class scoped_refptr {
     ptr_ = p;
     return *this;
   }
-
+  //赋值智能指针，新引用计数对象的引用计数+1，原来的-1
   scoped_refptr<T>& operator=(const scoped_refptr<T>& r) {
-    return *this = r.ptr_;
+    return *this = r.ptr_; //取出智能指针的内部引用计数的指针,利用重载赋值运算符的功能实现赋值
   }
-
+  //赋值T的子类U的智能指针，新引用计数对象的引用计数+1，原来的-1
   template <typename U>
   scoped_refptr<T>& operator=(const scoped_refptr<U>& r) {
-    return *this = r.get();
+    return *this = r.get();//使用get()方法取出智能指针的内部引用计数的指针，利用重载赋值运算符的功能实现赋值
   }
-
+  //移动赋值右值智能指针，新引用计数对象的引用计数不变，原来引用计数对象的引用计数不变
+  //使用移动语义std::move + 移动构造 + swap进行引用计数对象的地址交换
   scoped_refptr<T>& operator=(scoped_refptr<T>&& r) noexcept {
     scoped_refptr<T>(std::move(r)).swap(*this);
     return *this;
   }
-
+  //移动赋值T的子类U的右值智能指针，新引用计数对象的引用计数不变，原来引用计数对象的引用计数不变
+  //使用移动语义std::move + 移动构造 + swap进行引用计数对象的地址交换
   template <typename U>
   scoped_refptr<T>& operator=(scoped_refptr<U>&& r) noexcept {
     scoped_refptr<T>(std::move(r)).swap(*this);
     return *this;
   }
-
+  //地址交换函数. 引用计数对象的地址交换
   void swap(T** pp) noexcept {
     T* p = ptr_;
     ptr_ = *pp;
     *pp = p;
   }
-
+  //智能智能内部的引用计数对象的地址交换
   void swap(scoped_refptr<T>& r) noexcept { swap(&r.ptr_); }
 
  protected:
